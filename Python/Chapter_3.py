0b78acc2bb44e1f354d510874e805a140f043f05
Gyanaa-Vaibhav <57447831+Gyanaa-Vaibhav@users.noreply.github.com>
2020-06-26 21:00:41 +0530

    UPDATE

--- Python/Chapter_3.py
+++ Python/Chapter_3.py
@@ -29,6 +29,42 @@ print(f'Hi {Guest_list[3]} i invite you to my dinner')
 # can’t make the dinner, so you need to send out a new set of invitations.
 # You’ll have to think of someone else to invite.
 del Guest_list[1:3]
-
-print(Guest_list)
 Updates_list = ['Name5','Nmae6','Name7']
+Guest_list.append(Updates_list[0])
+Guest_list.append(Updates_list[1])
+Guest_list.append(Updates_list[2])
+
+print(f'Hi {Guest_list[4]} i invite you to my dinner')
+print(f'Hi {Guest_list[2]} i invite you to my dinner')
+print(f'Hi {Guest_list[3]} i invite you to my dinner')
+
+# 3-6. More Guests: You just found a bigger dinner table, so
+# now more space is available. Think of three more guests to invite to dinner.
+print(f'Hey {Guest_list[3]} {Guest_list[2]} {Guest_list[4]}'
+      f' {Guest_list[0]} {Guest_list[1]} i Have just bought a bgger '
+      f'table if you don\'t mind i will be inviting some other\'s too')
+
+Guest_list.insert(0, 'Name9')
+Guest_list.insert(1, 'Name10')
+Guest_list.append('Name11')
+print(Guest_list)
+
+#3-7. Shrinking Guest List: You just found out that your new dinner table
+# won’t arrive in time for the dinner, and you have space for only two guests.
+unknowm = [Guest_list.pop(1),
+Guest_list.pop(5),
+Guest_list.pop(2),
+Guest_list.pop(4),
+Guest_list.pop(-1),
+Guest_list.pop(0)
+]
+
+print(f'Sorry Yo\'re Not allowed in my dinner {unknowm[0]}')
+print(f'Sorry Yo\'re Not allowed in my dinner {unknowm[1]}')
+print(f'Sorry Yo\'re Not allowed in my dinner {unknowm[2]}')
+print(f'Sorry Yo\'re Not allowed in my dinner {unknowm[3]}')
+print(f'Sorry Yo\'re Not allowed in my dinner {unknowm[4]}')
+print(f'Sorry Yo\'re Not allowed in my dinner {unknowm[5]}')
+
+print(f'you are in the party {Guest_list[0]} congrats!!')
+print(f'you are in the party {Guest_list[1]} congrats!!')
