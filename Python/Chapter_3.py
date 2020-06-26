5768fe6781c371ef7298b3807155da4c4b8c8407
Gyanaa-Vaibhav <57447831+Gyanaa-Vaibhav@users.noreply.github.com>
2020-06-26 20:27:42 +0530

    updates

--- Python/Chapter_3.py
+++ Python/Chapter_3.py
@@ -15,3 +15,20 @@ Transportation = ['car', 'bike', 'Motorcycel']
 print(f"I would like to own a Honda {Transportation[1]}.")
 print(f'I would like to own a Honda {Transportation[2]}')
 print(f'I would like to own a Honda {Transportation[0]}')
+
+# 3-4. Guest List: If you could invite anyone, living or deceased
+# to dinner, who would you invite? Make a list that includes at
+# least three people you’d like to invite to dinner and print their name
+Guest_list = ['Nmae1', 'Name2', 'Name3', 'Name4']
+print(f'Hi {Guest_list[0]} i invite you to my dinner')
+print(f'Hi {Guest_list[1]} i invite you to my dinner')
+print(f'Hi {Guest_list[2]} i invite you to my dinner')
+print(f'Hi {Guest_list[3]} i invite you to my dinner')
+
+# 3-5. Changing Guest List: You just heard that one of your guests
+# can’t make the dinner, so you need to send out a new set of invitations.
+# You’ll have to think of someone else to invite.
+del Guest_list[1:3]
+
+print(Guest_list)
+Updates_list = ['Name5','Nmae6','Name7']
